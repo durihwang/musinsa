78dbf4ffb6325ca3d335eea19db898bbc2b53101
황두리 <hdr@gabia.com>
2022-12-28 18:00:14 +0900

    세팅

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # musinsa 재고 배송 시스템
+
+# 설치 방법
+## 빌드
+```shell
+ ./gradlew build
+```
+## 설치 및 실행
+```shell
+java -jar application-1.0-SNAPSHOT.jar
+```
--- application/src/main/java/org/musinsa/application/controller/ProductController.java
+++ application/src/main/java/org/musinsa/application/controller/ProductController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 import org.musinsa.application.dto.FindProductRequestDto;
 import org.musinsa.application.dto.FindProductResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     @GetMapping(value = "/product")
-    public FindProductResponseDto findProduct(@RequestBody @Valid FindProductRequestDto findProductRequestDto) {
+    public FindProductResponseDto findProduct(
+        @RequestBody @Valid FindProductRequestDto findProductRequestDto) {
 
         return null;
+    }
+
+    @PutMapping(value = "/product/{productName}/{optionName}/{quantity}")
+    public void updateProduct() {
+
     }
 }
